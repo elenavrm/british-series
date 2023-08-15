bf9d19e2013f611b240bd94365f9795afa87f000
Elena V <elenaveremchuk@Elenas-MacBook-Pro.local>
2023-08-14 22:08:16 -0500

    created and modified files' git push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,184 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { senseEight } from './data';
+import { peakyBlinders } from './data';
+import { sherlock } from './data';
 import './App.css';
 
 function App() {
+  
+
+  const [filmOne, setFilmOne] = useState(0);
+  const {idOne, nameOne, seasonOne, imageOne, descriptionOne} = senseEight[filmOne];
+  const [showMoreOne, setShowMoreOne] = useState(false);
+
+
+  const [filmTwo, setFilmTwo] = useState(0);
+  const { idTwo, nameTwo, seasonTwo, imageTwo, descriptionTwo} = peakyBlinders[filmTwo];
+  const [showMoreTwo, setShowMoreTwo] = useState(false);
+
+  const [filmThree, setFilmThree] = useState(0);
+  const { idThree, nameThree, seasonThree, imageThree, descriptionThree} = sherlock[filmThree];
+  const [showMoreThree, setShowMoreThree] = useState(false);
+
+  
+
+  const previousFilmOne = () => {
+    setFilmOne((film => {
+      film --;
+      if (film < 0) {
+        return senseEight.length -1;
+      }
+      return film;
+    }))
+
+  }
+
+  const nextFilmOne = () => {
+    setFilmOne((film => {
+      film ++;
+      if (film > senseEight.length -1) {
+        film = 0;
+      }
+      return film;
+    }))
+  }
+
+  const previousFilmTwo = () => {
+    setFilmTwo((filmOne => {
+      filmOne --;
+      if (filmOne < 0) {
+        return peakyBlinders.length -1
+      }
+      return filmOne;
+      
+    }))
+  }
+
+  const nextFilmTwo = () => {
+    setFilmTwo((filmOne => {
+      filmOne ++;
+      if (filmOne > peakyBlinders.length -1) {
+        filmOne = 0;
+      }
+      return filmOne;
+    }))
+  }
+
+  const previousFilmThree = () => {
+    setFilmThree((filmTwo => {
+      filmTwo --;
+      if (filmTwo < 0) {
+        return sherlock.length -1;
+      }
+      return filmTwo;
+    }))
+  }
+
+  const nextFilmThree = () => {
+    setFilmThree ((filmTwo => {
+      filmTwo ++;
+      if (filmTwo > sherlock.length -1) {
+        filmTwo = 0;
+      }
+      return filmTwo;
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+<div className='container'>
+  <h1> Top British Series</h1>
+</div>
+
+<div className="container">
+      <h1> {idOne} - {nameOne} </h1>
+    </div>
+
+    <div className='container'>
+      <img src={imageOne} width="300px" alt="series poster"/>
+    </div>
+
+    <div className='container'>
+      <h2>{seasonOne}</h2>
+    </div>
+
+    <div className='container'>
+      <h3>{showMoreOne ? descriptionOne : descriptionOne.substring(0,121) + "..."}
+      <button onClick={() => setShowMoreOne(!showMoreOne)}>{showMoreOne ? "show less" : "show more"}</button>
+      </h3>
+    </div>
+
+    <div className='container'>
+      <button onClick={previousFilmOne}> PREVIOUS </button>
+      <button onClick={nextFilmOne}> NEXT </button>
+    </div>
+
+    <div className='container'>
+      <button> DELETE </button>
+    </div>
+
+    <div className='container'>
+  <h1> {idTwo} - {nameTwo}</h1>
+    </div>
+
+    <div className='container'>
+      <img src={imageTwo} width ="300px" alt="series poster"/>
+    </div>
+
+    <div className='container'>
+      <h2>{seasonTwo}</h2>
+    </div>
+
+    <div className='container'>
+    <h3>{showMoreTwo ? descriptionTwo : descriptionTwo.substring(0,121) + "..."}
+      <button onClick={() => setShowMoreTwo(!showMoreTwo)}>{showMoreTwo ? "show less" : "show more"}</button>
+      </h3>
+    </div>
+
+    <div className='container'>
+      <button onClick={previousFilmTwo}> PREVIOUS </button>
+      <button onClick={nextFilmTwo}> NEXT </button>
+    </div>
+
+    <div className='container'>
+      <button> DELETE </button>
+    </div>
+
+    <div className='container'>
+      <h1>{idThree} - {nameThree}</h1>
+    </div>
+
+    <div className='container'>
+      <img src={imageThree} width="300px" alt="series poster"/>
+    </div>
+
+    <div className='container'>
+      <h2>{seasonThree}</h2>
+    </div>
+
+    <div className='container'>
+    <h3>{showMoreThree ? descriptionThree : descriptionThree.substring(0,120) + "..."}
+      <button onClick={() => setShowMoreThree(!showMoreThree)}>{showMoreThree ? "show less" : "show more"}</button>
+      </h3>
+    </div>
+
+    <div className='container'>
+      <button onClick={previousFilmThree}> PREVIOUS </button>
+      <button onClick={nextFilmThree}> NEXT </button>
+    </div>
+
+    <div className='container'>
+      <button> DELETE </button>
+    </div>
+
+    <div className='container'>
+      <button className="btn"> DELETE ALL </button>
+    </div>
+
     </div>
   );
+
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,98 @@
+
+
+export const senseEight = [
+    {
+        id: 1,
+        idOne: 1,
+        nameOne: "Sense 8",
+        seasonOne: "Season 1",
+        imageOne: "https://i.pinimg.com/originals/98/72/2c/98722c79362c1072aafb0f8aa2978f3c.png",
+        descriptionOne: "Season 1 of 'Sense8' follows the interconnected lives of eight individuals from different parts of the world who suddenly find themselves mentally and emotionally linked. They are called 'sensates' and share a psychic connection that allows them to experience each other's thoughts, emotions, and skills. The characters come from diverse backgrounds, cultures, and lifestyles, including a Chicago cop, a German safe-cracker, a Kenyan bus driver, an Icelandic DJ, a Korean businesswoman, an Indian pharmacist, a Mexican actor, and a transgender American hacker."
+    },
+
+    {
+        idOne: 2,
+        nameOne: "Sense 8",
+        seasonOne: "Season 2",
+        imageOne: "https://m.media-amazon.com/images/M/MV5BMjA4MTEyMzcwMl5BMl5BanBnXkFtZTgwMTIwODczNTM@._V1_FMjpg_UX1000_.jpg",
+        descriptionOne: "In the second season of 'Sense8' the story continues to follow eight individuals from around the world who are mentally and emotionally connected, forming a cluster known as sensates. These sensates possess the ability to share thoughts, emotions, skills, and even physical sensations across great distances. As they learn to navigate this unique connection, they become involved in a deeper conspiracy and face various challenges that test their bond."
+    }
+]
+
+export const peakyBlinders = [
+    {
+        id: 2,
+        idTwo: 1,
+        nameTwo: "Peaky Blinders",
+        seasonTwo: "Season 1",
+        imageTwo: "https://www.tallengestore.com/cdn/shop/products/PeakyBlinders-ThomasShelby-GarrisonBombing-NetflixTVShow-ArtPoster_a29a5be9-9611-43d9-b415-18655f60c629.jpg?v=1619864667",
+        descriptionTwo: "In Season 1, the story revolves around Thomas Shelby (played by Cillian Murphy), the cunning and ambitious leader of the Shelby crime family, named after their practice of sewing razor blades into the peaks of their flat caps. The Shelbys are known for their illegal activities such as gambling, bookmaking, and protection rackets. The first season introduces the audience to the family's activities, their conflicts with rival gangs, and their plans to expand their influence. As Thomas Shelby navigates the criminal underworld, he also has to deal with various personal challenges, political intrigue, and law enforcement efforts to bring down his organization."
+    },
+    {
+        idTwo: 2,
+        nameTwo: "Peaky Blinders",
+        seasonTwo: "Season 2",
+        imageTwo: "https://flxt.tmsimg.com/assets/p11094767_b_v10_ac.jpg",
+        descriptionTwo: "In Season 2, the Peaky Blinders gang, led by Thomas Shelby (played by Cillian Murphy), continues to expand their influence and criminal operations. The season delves deeper into the complexities of their relationships, both within the family and with rival factions. As with the first season, the series continues to showcase the family's struggles, alliances, and betrayals. Throughout the season, the Peaky Blinders face various challenges, including dealing with the law enforcement, rival gangs, and personal conflicts. The character development and interactions become more intricate as the story progresses."
+    },
+    {
+        idTwo: 3,
+        nameTwo: "Peaky Blinders",
+        seasonTwo: "Season 3",
+        imageTwo: "https://i.pinimg.com/474x/c6/86/09/c686096a1611f181b5212afad176e42d.jpg",
+        descriptionTwo: "Season 3 of 'Peaky Blinders' continues to explore the Shelby family's criminal activities and their efforts to expand their influence. In this season, Tommy Shelby, the family's charismatic leader, becomes embroiled in more complex schemes, alliances, and conflicts, all while facing new challenges that test his leadership. The season delves deeper into the characters' personal struggles, relationships, and the consequences of their actions. The blend of historical context, compelling characters, and intricate plotting makes 'Peaky Blinders' a popular and engaging series for viewers."
+    },
+    {
+        idTwo: 4,
+        nameTwo: "Peaky Blinders",
+        seasonTwo: "Season 4",
+        imageTwo: "https://wogifts.com/wp-content/uploads/2022/03/peaky-blinders-thomas-shelby-poster-wall_1648663581.jpg",
+        descriptionTwo: "In Season 4 of 'Peaky Blinders', the Shelby crime family faces new challenges and threats as they continue to expand their empire. The season picks up with Thomas Shelby (played by Cillian Murphy) and his family members attempting to establish themselves as legitimate business owners while still navigating the criminal underworld. As they strive to solidify their position, they encounter new adversaries, including the Italian mafia, led by Luca Changretta (played by Adrien Brody). The season is marked by power struggles, betrayals, and tense confrontations, as the Shelby family's loyalty and unity are put to the test. Thomas Shelby must rely on his wits and resourcefulness more than ever to protect his family and maintain control over their operations."
+    },
+    {
+        idTwo: 5,
+        nameTwo: "Peaky Blinders",
+        seasonTwo: "Season 5",
+        imageTwo: "https://i.pinimg.com/originals/19/33/cc/1933ccc4689e1f76c57cfbe83e8c0496.png",
+        descriptionTwo: "In Season 5, the Shelby family faces new challenges as they become entangled with political and economic forces. Tommy Shelby (played by Cillian Murphy) is now a Member of Parliament and continues to expand his influence and power. The season explores the impact of the Wall Street Crash of 1929 and its reverberations on the criminal world and the Shelby family's operations. As always, the series is characterized by its intricate plots, strong character development, and a blend of historical and fictional elements.Throughout the season, tensions rise within the family as they confront external threats and internal conflicts. The themes of power, loyalty, and betrayal remain central to the narrative, and viewers can expect a mix of action, drama, and suspense. The fifth season further delves into the complex dynamics of the Shelby family and the consequences of their actions."
+    },
+    {
+        idTwo: 6,
+        nameTwo: "Peaky Blinders",
+        seasonTwo: "Season 6",
+        imageTwo: "https://cdnb.artstation.com/p/assets/images/images/046/680/003/large/yas-koss-d6ad21138001053-6215214974718.jpg?1645690783",
+        descriptionTwo: "No spoilers for Season 6, watch it by yourself to see how the story ends..."
+    }
+]
+
+export const sherlock = [
+    {
+        id: 3,
+        idThree: 1,
+        nameThree: "Sherlock BBC",
+        seasonThree: "Season 1",
+        imageThree: "https://www.arthur-conan-doyle.com/images/5/57/Sherlock-BBC-poster-2.jpg",
+        descriptionThree: "'Sherlock' is a British television series produced by the BBC that premiered in 2010. The first season of the show is a modern-day adaptation of Sir Arthur Conan Doyle's classic detective stories featuring Sherlock Holmes and Dr. John Watson. In this season, Sherlock Holmes, played by Benedict Cumberbatch, is portrayed as a brilliant consulting detective with exceptional deductive reasoning skills, while Dr. John Watson, portrayed by Martin Freeman, is a war veteran who becomes Sherlock's loyal friend and partner in solving various complex cases."
+    },
+    {
+        idThree: 2,
+        nameThree: "Sherlock BBC",
+        seasonThree: "Season 2",
+        imageThree: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/50f59dfe-f3de-4253-98ed-41972905ee82/dd6bq53-810280dd-dd91-4ab2-b4fa-5bdf651d0473.jpg/v1/fill/w_734,h_1088,q_70,strp/sherlock__bbc____tv_series___poster_by_omrkse10_dd6bq53-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTg5NiIsInBhdGgiOiJcL2ZcLzUwZjU5ZGZlLWYzZGUtNDI1My05OGVkLTQxOTcyOTA1ZWU4MlwvZGQ2YnE1My04MTAyODBkZC1kZDkxLTRhYjItYjRmYS01YmRmNjUxZDA0NzMuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.WkXIXeUXKlp79wAEChfDtNflBPTfNHbRhKBNs_46ygc",
+        descriptionThree: "Season 2 of 'Sherlock' originally aired in 2012. It consists of three episodes, each adapting classic Sherlock Holmes stories with a modern twist. The season continues to follow the adventures of Sherlock Holmes (played by Benedict Cumberbatch) and his friend Dr. John Watson (played by Martin Freeman) as they solve complex and intriguing cases in contemporary London."
+    },
+    {
+        idThree: 3,
+        nameThree: "Sherlock BBC",
+        seasonThree: "Season 3",
+        imageThree: "https://flxt.tmsimg.com/assets/p10406404_b1t_v9_aa.jpg",
+        descriptionThree: "After the intense events of Season 2, which concluded with Sherlock's apparent death, Season 3 explores the aftermath of his return to London and the impact it has on his relationships and the people around him. Throughout this season, Sherlock and John face new challenges and mysteries, including a terrorist threat known as The Empty Hearse'. The show retains its signature style of intricate puzzles, sharp wit, and captivating storytelling. The chemistry between the main characters remains a highlight, and their dynamic evolves as they deal with personal and professional dilemmas."
+    },
+    {
+        idThree: 4,
+        nameThree: "Sherlock BBC",
+        seasonThree: "Season 4",
+        imageThree: "https://flxt.tmsimg.com/assets/p13517391_b1t_v8_aa.jpg",
+        descriptionThree: "Sherlock Holmes returns to Britain as Watson and his wife prepare for their first child. Sherlock faces perhaps the most chilling enemy of his long career -- the powerful and seemingly unassailable Culverton Smith, a man with a very dark secret. Watch it for yourself to see how the story ends..."
+    }
+]
