1e534898993eecb1dc954012402136d65cc94c8d
Elena V <elenaveremchuk@Elenas-MacBook-Pro.local>
2023-08-17 14:50:32 -0400

    modified existing file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { tvShows } from './data'
 import './App.css';
+import Season from './Season';
 
 
 
@@ -10,18 +11,17 @@ function App() {
       <div className="container">
         <h1>Top {tvShows.length} British Series</h1>
       </div>
-      {tvShows.map((show) => (
-        <div key={show.id}>
-          <div className="container">
-            <h2>
-              {show.id} - {show.name}
-            </h2>
+    {tvShows.map((element => {
+      const { id, name, seasons } = element;
+      return (
+        <div>
+          <div className='container'>
+            <h2>{id} - {name}</h2>
           </div>
-          
-   
-          
+          <Season seasons={seasons}/>
         </div>
-      ))}
+      )
+    }))}
     </div>
   );
 }
